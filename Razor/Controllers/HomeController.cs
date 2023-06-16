1d32d2d0f55108290f371e827cfc0367c310aa5c
duc-tai0404 <114871782+duc-tai0404@users.noreply.github.com>
2023-06-16 15:20:04 +0700

    Made some changes in Controller, View file.

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -1,26 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Razor.Models;
+using System;
 using System.Diagnostics;
 
 namespace Razor.Controllers
 {
     public class HomeController : Controller
     {
-        
+
         public IActionResult Index()
         {
-            Product product = new Product()
-            {
-                ProductId = 1,
-                ProductName = "Kayak",
-                ProductDescription = "This is decription the test!",
-                Category = "Watersports",
-                Price = 175M
-            };
-            ViewBag.StockLevel = 2;
-            return View(product);
-        }
+        //    Product product = new Product()
+        //    {
+        //        ProductId = 1,
+        //        ProductName = "Kayak",
+        //        ProductDescription = "This is decription the test!",
+        //        Category = "Watersports",
+        //        Price = 175M
+        //    };
+        Product[] array =
+        {
+            new Product { ProductName = "Kayak", Price = 275M },
+			new Product { ProductName = "Lifejacket", Price = 48.95M },
+			new Product { ProductName = "Soccer Ball", Price = 19.50M },
+			new Product { ProductName = "Corner Flag", Price = 34.95M }
 
-       
+		};
+        return View(array);
+        } 
     }
 }
